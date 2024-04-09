fe02b76c9bbcbf6f3a031de1e05cfd827f47367e
Gabrielle Tolentino <gabi.tolentino23@gmail.com>
2024-04-09 14:02:31 -0300

    Adição do arquivo data 09/04/24

---
+++ data/index.js
@@ -0,0 +1,54 @@
+const DATA =
+[
+    {
+      id: '01',
+      titulo: 'dormir',
+      descricao: 'Todos os dias eu durmo no mínimo 8h'  ,
+      porcentagem: '33,3%',
+      imagem: 'https://cdn-icons-png.flaticon.com/512/89/89977.png',
+    },
+    {
+        id: '02',
+        titulo: 'Estudar',
+        descricao: 'De segunda a sexta estudo na Etec, e reforço em casa'  ,
+        porcentagem: '16,7',
+        imagem: 'https://cdn-icons-png.flaticon.com/512/1124/1124988.png',
+      },
+      {
+        id: '03',
+        titulo: 'Saúde',
+        descricao: 'Todos os dias deixo um momento pra cuidar da minha saúde física e mental.'  ,
+        porcentagem: '10,4',
+        imagem: 'https://cdn-icons-png.flaticon.com/512/5455/5455384.png',
+      },
+      {
+        id: '04',
+        porcentagem: '10,4',
+        titulo:'Cantar',
+        descricao:'Não pode faltar no dia, canto enquanto faço as tarefas de casa.',
+        imagem: 'https://static.vecteezy.com/system/resources/thumbnails/014/391/889/small/microphone-icon-on-transparent-background-microphone-icon-free-png.png',
+      },
+      {
+        id: '05',
+        titulo: 'Lazer',
+        descricao: 'Todos os dias tenho um momento de descanso, onde leio um livro ou fico com minha família.'  ,
+        porcentagem: '12,6%',
+        imagem: 'https://cdn-icons-png.flaticon.com/512/81/81403.png',
+      },
+      {
+        id: '06',
+        titulo: 'Comer',
+        descricao: 'Todos os dias eu tomo café da manhã, almoço e janto.'  ,
+        porcentagem: '16,7',
+        imagem: 'https://cdn-icons-png.flaticon.com/512/857/857681.png',
+      },
+      {
+        id: '07',
+        titulo: 'Lazer',
+        descricao: 'Todos os dias tenho um momento de descanso, onde leio um livro ou fico com minha família.'  ,
+        porcentagem: '12,6%',
+        imagem: 'https://cdn-icons-png.flaticon.com/512/81/81403.png',
+      },
+]
+
+export default DATA
